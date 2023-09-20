e326efad19cc242435c43f223c39c9f90b6204c8
Anton Reiswich <anton.reiswich@dlr.de>
2023-09-20 12:49:29 +0200

    cleaned up the code

--- examples/test_main_2.cpp
+++ examples/test_main_2.cpp
@@ -1,64 +1,19 @@
-
-#include <geoml/curves/modeling.h>
-
 #include <iostream>
-
 #include <cmath>
 #include <vector>
-/*
-#include <initializer_list>
 
-#include "geolib/BSplineCurve.hpp"
-#include "geolib/CurvesToSurface.hpp"
-#include "geolib/PointsToBSplineInterpolation.hpp"
-
-#include "geolib/WritingMethods.hpp"
-#include "geolib/BSplineAlgorithms.hpp" 
-
-#include "Transformation.hpp"
-
-#include "filletEdge.hpp"
-*/
+#include <geoml/curves/modeling.h>
 
 #include <gp_Pnt.hxx>
 #include <TColgp_HArray1OfPnt.hxx>
 #include <Geom_BSplineCurve.hxx>
 #include "Geom_Curve.hxx"
-
-/*
-#include "BRepPrimAPI_MakeBox.hxx"
-#include "BRepGProp.hxx"
-#include "GProp_GProps.hxx"
-#include <GeomAPI_Interpolate.hxx>
-
-#include "Geom_Surface.hxx"
-
-
-#include <GeomFill_BSplineCurves.hxx>
-#include <GeomFill_Coons.hxx>
-#include <BRepTools.hxx>
-#include <BRepBuilderAPI_MakeEdge.hxx>
-#include <TopoDS_Shape.hxx> 
-#include <TopoDS.hxx> 
-#include <TopoDS_Solid.hxx> 
-#include <Geom_BSplineSurface.hxx>
-#include <Geom_TrimmedCurve.hxx>
-
-#include <gp_GTrsf.hxx>
-
-#include <gp_Mat.hxx>
-
-#include <gp_XYZ.hxx>
-
-
-using namespace geo;
-*/
-
-
 #include "BRepBuilderAPI_MakeEdge.hxx"
 #include <TopoDS_Shape.hxx>
 #include "STEPControl_Writer.hxx"
 
+
+// define a function that writes curves to step files:
 void writeToStp(Handle_Geom_Curve cur, std::string const& name_file)
 {
 BRepBuilderAPI_MakeEdge edgeMaker;
@@ -72,12 +27,9 @@ aStepWriter.Write(name_file.c_str());
 
 int main(){
 
-    std::cout << "Try if this works..." << std::endl;
-
-
-////
-//// interplate NACA points to a B-spline curve:
-////
+/////////////////////////////////////////////////////
+//// interplate NACA points to a B-spline curve: ////
+/////////////////////////////////////////////////////
 
 // list of 21 points on NACA2412 profile:
 std::vector<double> px {1.000084, 0.975825, 0.905287, 0.795069, 0.655665, 0.500588, 0.34468, 0.203313, 0.091996, 0.022051, 0.0, 0.026892, 0.098987, 0.208902, 0.346303, 0.499412, 0.653352, 0.792716, 0.90373, 0.975232, 0.999916};
@@ -98,58 +50,6 @@ Handle(Geom_BSplineCurve) mySplineNACA_Geom_Curve
 // write curve to .stp file
 writeToStp(mySplineNACA_Geom_Curve, "new_test_interpolation_curve.stp");
 
-/*
-
-// define transformations of profiles: 
-gp_Trsf first_traf;
-first_traf.SetValues(100.,0.,0.,0.,
-					0., 100., 0., 0.,
-					0., 0., 1., 0.);
-
-
-Transformation my_first_trafo (first_traf);
-
-gp_Trsf second_traf;
-second_traf.SetValues(80.,0.,0.,0.,
-					0., 100., 0., 0.,
-					0., 0., 1., 50.);
-
-Transformation my_second_trafo (second_traf);
-
-gp_Trsf third_traf;
-third_traf.SetValues(50.,0.,0.,0.,
-					0., 100., 0., 20.,
-					0., 0., 1., 100.);
-
-Transformation my_third_trafo (third_traf);
-
-// now create the three curves via the defined three transformations
-
-Handle_Geom_Curve my_first_transformed_profile = transform(interpolatorNACA.Curve(), my_first_trafo);
-Handle_Geom_Curve my_second_transformed_profile = transform(interpolatorNACA.Curve(), my_second_trafo);
-Handle_Geom_Curve my_third_transformed_profile = transform(interpolatorNACA.Curve(), my_third_trafo);
-
-// write
-writeToStp(my_first_transformed_profile, "my_first_transformed_profile.stp");
-writeToStp(my_second_transformed_profile, "my_second_transformed_profile.stp");
-writeToStp(my_third_transformed_profile, "my_third_transformed_profile.stp");
-
-// now create the wing surface from the transformed NACA profiles:
-std::vector<Handle(Geom_Curve)> vec_geom_NACA_crvs {my_first_transformed_profile,
-	my_second_transformed_profile, my_third_transformed_profile};
-
-// create the surface via the free function interpolate_curves (in CurvesToSurface.hpp):
-Handle(Geom_BSplineSurface) result_surf_wing = interpolate_curves(vec_geom_NACA_crvs);
-
-writeGeomEntityToStepFile(result_surf_wing, "surface_wing.stp");
-
-//// we reached cp:2 (cf. diagram in Miro)
-
-////
-//// 
-////
-
-*/
 return 0;
 }
 
