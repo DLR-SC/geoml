b0d325f83a325511052bf48ccaf2a694803d6d32
Anton Reiswich <anton.reiswich@dlr.de>
2023-09-25 14:24:48 +0200

    add unit tests for the creation of simple B-spline curves

--- src/geoml/curves/modeling.h
+++ src/geoml/curves/modeling.h
@@ -22,6 +22,33 @@ namespace geoml
  * @param points Multiple points that will be interpolated
  */
 GEOML_API_EXPORT Handle(Geom_BSplineCurve)
-interpolate_points_to_b_spline_curve(const Handle(TColgp_HArray1OfPnt) & points); //start with a simple version to try it out first
+interpolate_points_to_b_spline_curve(const Handle(TColgp_HArray1OfPnt) & points);
+
+
+/**
+ * @brief Define a B-spline curve by setting the control points, the weights, the degree,
+ * the knots, the multiplicities
+ * 
+ * 
+ * @param control_points The control points of the B-spline curve
+ * @param weights The weights of the B-spline curve
+ * @param knots The knots of the B-spline curve, knots with multiplicity larger than 1 only noted once 
+ * @param multiplicities The multiplicities of the knots
+ * @param degree The degree of the B-spline curve
+ * @param periodic ### to do: understand this better! Test it ###
+ * @param check ### to do: test this ####
+ */
+GEOML_API_EXPORT Geom_BSplineCurve
+b_spline_curve(
+    const TColgp_Array1OfPnt & control_points, 
+    const TColStd_Array1OfReal & weights,
+    const TColStd_Array1OfReal & knots, 
+    const TColStd_Array1OfInteger & multiplicities,
+    const int degree, 
+    const bool periodic = false, 
+    const bool checkRational = false);
+
+
+
 
 } // namespace geoml
