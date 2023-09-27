00ea3979ca8f32f0eb6975ae06f215ae9c548d00
Anton Reiswich <anton.reiswich@dlr.de>
2023-09-27 16:38:58 +0200

    extend example to try out some functionality

--- examples/fuselage_wing_configuration.cpp
+++ examples/fuselage_wing_configuration.cpp
@@ -10,6 +10,7 @@
 #include <Geom_BSplineCurve.hxx>
 #include "Geom_Curve.hxx"
 #include "BRepBuilderAPI_MakeEdge.hxx"
+#include "BRepBuilderAPI_MakeFace.hxx"
 #include <TopoDS_Shape.hxx>
 #include "STEPControl_Writer.hxx"
 
@@ -23,7 +24,20 @@
 #include "BRepGProp.hxx"
 #include "GProp_GProps.hxx"
 
+#include <Geom_Circle.hxx>
+#include <gp_Ax2.hxx>
 
+#include <TopExp_Explorer.hxx>
+#include <TopoDS_Edge.hxx>
+#include <BRep_Tool.hxx>
+
+#include <TopTools_IndexedMapOfShape.hxx>
+#include <TopExp.hxx>
+#include <TopAbs.hxx>
+#include <TopoDS.hxx>
+
+#include <BRepAdaptor_Curve.hxx>
+#include <gp_Circ.hxx>
 
 // define a function that writes curves to step files:
 void writeToStp(Handle_Geom_Curve cur, std::string const& name_file)
@@ -35,9 +49,9 @@ TopoDS_Shape edge = edgeMaker.Shape();
 STEPControl_Writer aStepWriter;
 aStepWriter.Transfer(edge,STEPControl_AsIs);
 aStepWriter.Write(name_file.c_str());
-}
+} 
 
-/*
+
 void writeGeomEntityToStepFile(Handle_Geom_Surface surface, std::string fileName)
 {
 	BRepBuilderAPI_MakeFace faceMaker;
@@ -49,7 +63,7 @@ void writeGeomEntityToStepFile(Handle_Geom_Surface surface, std::string fileName
 	writer.Write(fileName.c_str());
 	return;
 }
-*/
+
 
 void writeGeomEntityToStepFile(const TopoDS_Shape& my_shape, std::string fileName)
 {
@@ -167,12 +181,11 @@ Handle(Geom_BSplineCurve) my_Geom_Curve_1
 
 writeToStp(my_Geom_Curve_1, "test_curve_1.stp");
 
-
 // now create a cylindrical surface:
 
 // create a profile curve for the cylinder
-gp_Pnt start_point(1, 0, 0);
-gp_Pnt end_point(1, 0, 2); // The height of the cylinder
+gp_Pnt start_point(3, 0, 0);
+gp_Pnt end_point(3, 0, 2); // The height of the cylinder
 
 TColgp_Array1OfPnt profile_points(1,2);
 profile_points.SetValue(1,start_point);
@@ -196,9 +209,14 @@ TColStd_Array1OfInteger mults(1,2);
 mults.SetValue(1,2);                
 mults.SetValue(2,2);
 
+/*
 // create the profile curve:
 Handle(Geom_BSplineCurve) profile_curve 
         = new Geom_BSplineCurve(profile_points, weights, knots, mults, degree);
+*/
+
+Handle(Geom_BSplineCurve) profile_curve 
+	= geoml::b_spline_curve(profile_points, weights, knots, mults, degree);
 
 // convert the Geom_BSplineCurve in a TopoDS_Shape:
 BRepBuilderAPI_MakeEdge edgeMaker;
@@ -206,7 +224,9 @@ edgeMaker.Init(profile_curve);
 TopoDS_Shape profile_curve_shape = edgeMaker.Shape();
 
 // create the cylinder by revolving the profile curve
-BRepPrimAPI_MakeRevol revol_maker(profile_curve_shape, gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1)), 3.1415); 
+BRepPrimAPI_MakeRevol revol_maker(	profile_curve_shape, 
+									gp_Ax1(gp_Pnt(0, 0, 0), 
+									gp_Dir(0, 0, 1))); 
 
 TopoDS_Shape cylinder = revol_maker.Shape();
 
@@ -216,7 +236,7 @@ writeGeomEntityToStepFile(cylinder, "my_cylinder_test.stp");
 
 std::cout << "hello hello from the other side" << std::endl;
 
- // calculate the area of the surface
+ // calculate the area of the surface // this can be a geoml function
  //TopoDS_Face face = BRepBuilderAPI_MakeFace(result_surf, 1e-5);
  GProp_GProps properties;
  BRepGProp::SurfaceProperties(cylinder, properties);
@@ -224,6 +244,185 @@ std::cout << "hello hello from the other side" << std::endl;
  double area = properties.Mass();
 
 std::cout << " the surface area is: " << area << std::endl;
+
+////////////////////////////////////////////////////////////////
+// In the next step, I want to extract one edge of the surface
+// of revolution and define one more closed curve and finally
+// loft them to a surface (e.g. by using the Gordon algorithm)
+////////////////////////////////////////////////////////////////
+
+// define the center of one more circle:
+gp_Pnt circle_center(0.0, 0.3, 0.0);
+
+// define the radius of the circle:
+double radius = 0.5;
+
+Geom_Circle my_circle(gp_Ax2(circle_center, gp_Dir(0,0,1), gp_Dir(1,0,0)), radius);
+
+std::cout << "The radius is :" << my_circle.Radius() << std::endl;
+
+Handle(Geom_Curve) my_circle_curve = 
+	new Geom_Circle(gp_Ax2(circle_center, gp_Dir(0,0,1), gp_Dir(1,0,0)), radius);
+
+writeToStp(my_circle_curve, "test_circle.stp");
+
+// now, extract the edge of the surface of revolution: (might encapsulate the following
+// in a function)
+int counter_variable(0);
+TopTools_IndexedMapOfShape edgeMap;
+TopExp::MapShapes(cylinder, TopAbs_EDGE, edgeMap);
+for(int i = 1; i <= edgeMap.Extent(); ++i)
+{
+   // get current edge and convert to TopoDS_Face
+   const TopoDS_Edge& edge = TopoDS::Edge(edgeMap(i));
+   counter_variable +=1;
+}
+std::cout << "The number of edges is: " << counter_variable << std::endl;
+
+writeGeomEntityToStepFile(TopoDS::Edge(edgeMap(2)), "my_test_edge.stp");// index 2: upper
+																		// circle
+																		// index 1: lower
+																		// circle
+																		// index: 3 vertical
+																		// line
+
+// now, as I managed to extract the edges of the surface of revolution, I will try
+// to interpolate one extracted edge with the preciously defined circle
+
+TopoDS_Edge my_edge = TopoDS::Edge(edgeMap(2));
+BRepAdaptor_Curve curve(my_edge);
+
+std::cout << "beginning - type output" << std::endl;
+std::cout << curve.GetType() << std::endl;
+std::cout << "end - type output" << std::endl;
+
+Handle(Geom_Curve) my_extracted_circle_curve = new Geom_Circle(curve.Circle());
+writeToStp(my_extracted_circle_curve, "test_extracted_circle_curve.stp");
+
+// now, interplate the two circles with a surface:
+std::vector<Handle(Geom_Curve)> my_curves;
+my_curves.push_back(my_extracted_circle_curve);
+my_curves.push_back(my_circle_curve);
+
+// the following line of code doesn't work yet: hence commented out
+//Handle(Geom_BSplineSurface) my_free_form_surface_loft = geoml::interpolate_curves(my_curves);
+std::cout << "hope till here" << std::endl;
+
+// writeGeomEntityToStepFile(my_free_form_surface_loft, "my_free_form_loft_surface.stp");
+
+// let us test the geoml::interpolate_curves function first with a simple example:
+
+// define the first curve:
+gp_Pnt p_a1(0.0, 0.0, 4.0);
+gp_Pnt p_a2(0.0, 1.0, 3.0);
+gp_Pnt p_a3(0.0, 2.0, 2.0);
+gp_Pnt p_a4(0.0, 3.0, 1.0);
+gp_Pnt p_a5(0.0, 4.0, 0.0);
+
+TColgp_Array1OfPnt control_points_a(1,5);
+
+control_points_a.SetValue(1,p_a1);
+control_points_a.SetValue(2,p_a2);
+control_points_a.SetValue(3,p_a3);
+control_points_a.SetValue(4,p_a4);
+control_points_a.SetValue(5,p_a5);
+
+// degree:
+Standard_Integer degree_a = 2;
+
+// weights:
+TColStd_Array1OfReal weights_a(1, 5);
+weights_a.SetValue(1, 1.0);  
+weights_a.SetValue(2, 1.0);  
+weights_a.SetValue(3, 1.0);  
+weights_a.SetValue(4, 1.0);
+weights_a.SetValue(5, 1.0);
+
+// knots:
+TColStd_Array1OfReal knots_a(1,4); 
+knots_a.SetValue(1,0.0);           
+knots_a.SetValue(2,1.0);
+knots_a.SetValue(3,2.0);
+knots_a.SetValue(4,3.0);
+
+// multiplicities: 
+TColStd_Array1OfInteger mults_a(1,4); 
+mults_a.SetValue(1,3);                
+mults_a.SetValue(2,1);
+mults_a.SetValue(3,1);
+mults_a.SetValue(4,3);
+
+Handle(Geom_BSplineCurve) my_Geom_Curve_a 
+        = new Geom_BSplineCurve(control_points_a, weights_a, knots_a, mults_a, degree_a);
+
+// define the second curve:
+gp_Pnt p_b1(5.0, 1.0, 3.0);
+gp_Pnt p_b2(5.0, 3.0, 5.0);
+gp_Pnt p_b3(5.0, 5.0, 3.0);
+gp_Pnt p_b4(5.0, 2.0, 3.0);
+gp_Pnt p_b5(5.0, 0.0, -1.0);
+
+TColgp_Array1OfPnt control_points_b(1,5);
+
+control_points_b.SetValue(1,p_b1);
+control_points_b.SetValue(2,p_b2);
+control_points_b.SetValue(3,p_b3);
+control_points_b.SetValue(4,p_b4);
+control_points_b.SetValue(5,p_b5);
+
+// degree:
+Standard_Integer degree_b = 2;
+
+// weights:
+TColStd_Array1OfReal weights_b(1, 5);
+weights_b.SetValue(1, 1.0);  
+weights_b.SetValue(2, 1.0);  
+weights_b.SetValue(3, 1.0);  
+weights_b.SetValue(4, 1.0);
+weights_b.SetValue(5, 1.0);
+
+// knots:
+TColStd_Array1OfReal knots_b(1,4); 
+knots_b.SetValue(1,0.0);           
+knots_b.SetValue(2,1.0);
+knots_b.SetValue(3,2.0);
+knots_b.SetValue(4,3.0);
+
+// multiplicities: 
+TColStd_Array1OfInteger mults_b(1,4); 
+mults_b.SetValue(1,3);                
+mults_b.SetValue(2,1);
+mults_b.SetValue(3,1);
+mults_b.SetValue(4,3);
+
+Handle(Geom_BSplineCurve) my_Geom_Curve_b 
+        = new Geom_BSplineCurve(control_points_b, weights_b, knots_b, mults_b, degree_b);
+
+// put both curves in one vector:
+std::vector<Handle(Geom_Curve)> my_test_curve_vector;
+my_test_curve_vector.push_back(my_Geom_Curve_a);
+my_test_curve_vector.push_back(my_Geom_Curve_b);
+
+// now, interpolate these two curves with a surface:
+Handle(Geom_BSplineSurface) my_test_interp_surface = 
+	geoml::interpolate_curves(my_test_curve_vector);
+
+// write a step file of the surface to disk:
+writeGeomEntityToStepFile(my_test_interp_surface, "free_surface.stp"); // this works
+
+// now, see if the interpolation of curves also works with tow Geom_Circles (defined above):
+
+// put both curves in one vector:
+std::vector<Handle(Geom_Curve)> my_test_circle_curve_vector;
+my_test_circle_curve_vector.push_back(my_circle_curve);
+my_test_circle_curve_vector.push_back(my_extracted_circle_curve);
+
+// now, interpolate these two curves with a surface:
+Handle(Geom_BSplineSurface) my_test_circle_interp_surface = 
+	geoml::interpolate_curves(my_test_circle_curve_vector);
+std::cout << "reached! till here!" << std::endl;
+// write a step file of the surface to disk:
+writeGeomEntityToStepFile(my_test_circle_interp_surface, "free_circ_surface.stp"); 
 
 
 return 0;
