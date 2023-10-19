3c4b1a1e57e6c0c232bdc672051fcb7024f7f3d6
Anton Reiswich <anton.reiswich@dlr.de>
2023-10-19 16:19:40 +0200

    nwe geoml functions and appropriate example file

--- src/geoml/surfaces/modeling.h
+++ src/geoml/surfaces/modeling.h
@@ -9,8 +9,11 @@
 #include <Geom_Curve.hxx>
 #include <geoml/geoml.h>
 
+#include <gp_Ax1.hxx>
+#include <Geom_BSplineCurve.hxx>
 
 #include <vector>
+#include <cmath>
 
 namespace geoml
 {
@@ -34,7 +37,6 @@ interpolate_curve_network(const std::vector<Handle(Geom_Curve)>& ucurves,
                           const std::vector<Handle(Geom_Curve)>& vcurves,
                           double tolerance);
 
-
 /**
  * @brief Interpolates the curve network by a bspline curve in v-direction
  *
@@ -47,5 +49,47 @@ GEOML_API_EXPORT Handle(Geom_BSplineSurface)
 interpolate_curves(const std::vector<Handle(Geom_Curve)>& curves,
                    unsigned int max_degree=3,
                    bool join_continously=false);
+
+/**
+ * @brief Create a revolving surface
+ *
+ * //@param profile_curve Profile curve (contained in a plane)
+ * //@param rotation_axis The rotation axis
+ * //@param angle The rotation angle (2*PI by default)
+ */
+
+GEOML_API_EXPORT Handle(Geom_BSplineSurface)
+revolving_surface(const Handle(Geom_BSplineCurve)& profile_curve,
+                  const gp_Ax1& rotation_axis,
+                  const Standard_Real angle=2*M_PI); // check if it works to choose another
+                                                     // angle
+
+/**
+ * @brief Create a B-spline surface
+ *
+ * //@param control_points A 2-dimensional control point net
+ * //@param weights A 2-dimensional weights net 
+ * //@param U_knots The knot vector in U-direction
+ * //@param V_knots The knot vector in V-direction
+ * //@param U_mults The multiplicities of the U-direction knots
+ * //@param V_mults The multiplicities of the V-direction knots
+ * //@param U_degree The degree in U-direction
+ * //@param V_degree The degree in V-direction
+ * //@param U_periodic Boolean flag for periodicity in U-direction
+ * //@param V_periodic Boolean flag for periodicity in V-direction
+ */
+
+GEOML_API_EXPORT Handle(Geom_BSplineSurface)
+b_spline_surface(const TColgp_Array2OfPnt &control_points, 
+                 const TColStd_Array2OfReal &weights, 
+                 const TColStd_Array1OfReal &U_knots, 
+                 const TColStd_Array1OfReal &V_knots, 
+                 const TColStd_Array1OfInteger &U_mults, 
+                 const TColStd_Array1OfInteger &V_mults, 
+                 const Standard_Integer U_degree, 
+                 const Standard_Integer V_degree, 
+                 const Standard_Boolean U_periodic=Standard_False, 
+                 const Standard_Boolean V_periodic=Standard_False); 
+                                     
 
 } // namespace geoml
--- tests/apitests/src/testSurfaceModeling.cpp
+++ tests/apitests/src/testSurfaceModeling.cpp
@@ -1,4 +1,5 @@
 #include <geoml/surfaces/modeling.h>
+#include <geoml/curves/modeling.h>
 
 #include <gtest/gtest.h>
 
@@ -13,10 +14,14 @@
 #include <TopExp_Explorer.hxx>
 #include <TopoDS.hxx>
 #include <TopoDS_Edge.hxx>
+#include "BRepGProp.hxx"
+#include "GProp_GProps.hxx"
 
 #include <filesystem>
-
 #include <iostream>
+
+#include "STEPControl_Writer.hxx"
+
 
 namespace apitests
 {
@@ -150,8 +155,188 @@ INSTANTIATE_TEST_SUITE_P(SurfaceModeling, interpolate_curves, ::testing::Values(
   "ffd"
 ));
 
-TEST(simple_test, testSimple)
+void writeGeomEntityToStepFile(Handle_Geom_Surface surface, std::string fileName)
 {
-//std::cout << " Write this line TEST" << std::endl;
-    //EXPECT_EQ(4,4);
+	BRepBuilderAPI_MakeFace faceMaker;
+	faceMaker.Init(surface,true,1E-6);	
+	TopoDS_Shape face = faceMaker.Shape();
+
+	STEPControl_Writer writer;
+	writer.Transfer(face,STEPControl_AsIs);
+	writer.Write(fileName.c_str());
+	return;
 }
+
+TEST(SurfaceModeling, revolution_surface)
+{
+    // create a cylindrical surface:
+
+    // create a profile curve for the cylinder
+    gp_Pnt start_point(7., 0., 2.);
+    gp_Pnt end_point(23., 0., 2.); // The height of the cylinder
+
+    TColgp_Array1OfPnt profile_points(1,2);
+    profile_points.SetValue(1,start_point);
+    profile_points.SetValue(2,end_point);
+
+    // degree:
+    Standard_Integer degree = 1;
+
+    // weights:
+    TColStd_Array1OfReal weights(1, 2);
+    weights.SetValue(1, 1.0);  
+    weights.SetValue(2, 1.0);  
+
+    // knots:
+    TColStd_Array1OfReal knots(1,2); 
+    knots.SetValue(1,0.0);           
+    knots.SetValue(2,1.0);
+
+    // multiplicities: 
+    TColStd_Array1OfInteger mults(1,2); 
+    mults.SetValue(1,2);                
+    mults.SetValue(2,2);
+
+    Handle(Geom_BSplineCurve) profile_curve 
+    	= geoml::b_spline_curve(profile_points, weights, knots, mults, degree);
+
+
+    Handle(Geom_BSplineSurface) cylinder =
+        geoml::revolving_surface(profile_curve, 
+                                 gp_Ax1(gp_Pnt(0., 0., 0.), gp_Dir(1., 0., 0.)));
+
+    // Create a TopoDS_Face using BRepBuilderAPI_MakeFace
+    BRepBuilderAPI_MakeFace face_maker(cylinder, 1.0e-6); 
+    TopoDS_Face face = face_maker.Face();
+    TopoDS_Shape shape = face;
+
+    // calculate the area of the surface 
+    GProp_GProps properties;
+    BRepGProp::SurfaceProperties(shape, properties);
+
+    double area = properties.Mass();
+
+    EXPECT_NEAR(area, 201.601, 1e-3);
+    EXPECT_TRUE(cylinder->IsVRational());
+    EXPECT_FALSE(cylinder->IsURational());
+    EXPECT_EQ(cylinder->NbUPoles(),6);
+    EXPECT_EQ(cylinder->NbVPoles(),2);
+    EXPECT_EQ(cylinder->UDegree(),2);
+    EXPECT_EQ(cylinder->VDegree(),1);
+    EXPECT_EQ(cylinder->NbUKnots(),4);
+    EXPECT_EQ(cylinder->NbVKnots(),2);
+    EXPECT_TRUE(cylinder->IsUPeriodic());
+    EXPECT_FALSE(cylinder->IsVPeriodic());
+
+    //writeGeomEntityToStepFile(cylinder, "cylinder_periodic.stp");
+
+    cylinder->SetUNotPeriodic();
+    EXPECT_FALSE(cylinder->IsUPeriodic());
+    EXPECT_EQ(cylinder->NbUPoles(),7); // this is important to note: it changed from 6 to 7
+    EXPECT_EQ(cylinder->NbUKnots(),6); // changed from 4 to 6 (why?)
+
+    //writeGeomEntityToStepFile(cylinder, "cylinder_non_periodic.stp");
+        
+}
+
+TEST(SurfaceModeling, b_spline_surface)
+{
+    int num_rows = 3;   //    ** 
+    int num_cols = 2;   //    **  
+                        //    **          
+
+    TColgp_Array2OfPnt srf_cp_net(1, num_rows, 1, num_cols);
+
+    // set the degree in U direction and V direction
+    Standard_Integer degree_U = 2;
+    Standard_Integer degree_V = 1;
+
+    EXPECT_EQ(srf_cp_net.ColLength(), 3);
+    EXPECT_EQ(srf_cp_net.RowLength(), 2);
+/*
+    // define the weight net for the surface:
+    TColStd_Array2OfReal srf_weight_net(1, srf_cp_net.ColLength(), 1, 
+                                                    rear_srf_cp_net.RowLength()); // is this use correct?
+
+    rear_srf_weight_net.SetValue(1, 1, 1.0);
+    rear_srf_weight_net.SetValue(2, 1, 0.5);
+    rear_srf_weight_net.SetValue(3, 1, 1.0);
+    rear_srf_weight_net.SetValue(4, 1, 0.5);
+    rear_srf_weight_net.SetValue(5, 1, 1.0);
+    rear_srf_weight_net.SetValue(6, 1, 0.5);
+
+    rear_srf_weight_net.SetValue(1, 2, 1.0);
+    rear_srf_weight_net.SetValue(2, 2, 0.5);
+    rear_srf_weight_net.SetValue(3, 2, 1.0);
+    rear_srf_weight_net.SetValue(4, 2, 0.5);
+    rear_srf_weight_net.SetValue(5, 2, 1.0);
+    rear_srf_weight_net.SetValue(6, 2, 0.5);
+
+    rear_srf_weight_net.SetValue(1, 3, 1.0);
+    rear_srf_weight_net.SetValue(2, 3, 0.5);
+    rear_srf_weight_net.SetValue(3, 3, 1.0);
+    rear_srf_weight_net.SetValue(4, 3, 0.5);
+    rear_srf_weight_net.SetValue(5, 3, 1.0);
+    rear_srf_weight_net.SetValue(6, 3, 0.5);
+
+    rear_srf_weight_net.SetValue(1, 4, 1.0);
+    rear_srf_weight_net.SetValue(2, 4, 0.5);
+    rear_srf_weight_net.SetValue(3, 4, 1.0);
+    rear_srf_weight_net.SetValue(4, 4, 0.5);
+    rear_srf_weight_net.SetValue(5, 4, 1.0);
+    rear_srf_weight_net.SetValue(6, 4, 0.5);
+
+
+    rear_srf_weight_net.SetValue(1, 5, 1.0);
+    rear_srf_weight_net.SetValue(2, 5, 0.5);
+    rear_srf_weight_net.SetValue(3, 5, 1.0);
+    rear_srf_weight_net.SetValue(4, 5, 0.5);
+    rear_srf_weight_net.SetValue(5, 5, 1.0);
+    rear_srf_weight_net.SetValue(6, 5, 0.5);
+
+
+    // set the U-knots (columns):
+    TColStd_Array1OfReal knots_U(1,4); 
+
+    knots_U.SetValue(1,0.0);           
+    knots_U.SetValue(2,1.0);
+    knots_U.SetValue(3,2.0);
+    knots_U.SetValue(4,3.0);
+
+    // set the V-knots (rows):
+    TColStd_Array1OfReal knots_V(1,2); 
+
+    knots_U.SetValue(1,0.0);           
+    knots_U.SetValue(2,1.0);
+
+    // multiplicities U-direction: 
+    TColStd_Array1OfInteger mults_U(1,4); // as we are in the periodic case, the mults must 	
+                                        // maybe be adapted...(!)
+    mults_U.SetValue(1,3);                
+    mults_U.SetValue(2,2);
+    mults_U.SetValue(3,2);
+    mults_U.SetValue(4,3);
+
+    // multiplicities V-direction: 
+    TColStd_Array1OfInteger mults_V(1,2); 
+
+    mults_V.SetValue(1,5);                
+    mults_V.SetValue(2,5);
+
+    std::cout << "does it get till here?" << std::endl;
+
+    // define the rear surface:
+    Handle(Geom_BSplineSurface) rear_surface
+        = geoml::b_spline_surface( rear_srf_cp_net, 
+                            rear_srf_weight_net, 
+                            knots_U, 
+                            knots_V, 
+                            mults_U, 
+                            mults_V, 
+                            degree_U, 
+                            degree_V, 
+                            Standard_True ); 
+*/					
+}
+
+
