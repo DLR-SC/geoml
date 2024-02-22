11d396f15fb343d3186a0d1b16647c21d0eb3902
Anton Reiswich <anton.reiswich@dlr.de>
2024-02-22 16:39:11 +0100

    remove all new geometry functionality

--- src/geoml/surfaces/modeling.h
+++ src/geoml/surfaces/modeling.h
@@ -19,7 +19,7 @@ namespace geoml
 {
 
 /**
- * @brief Interpolates the curve network by a bspline curve
+ * @brief Interpolates the curve network by a bspline surface
  * 
  * This uses the Gordon interpolation method.
  * The u curves and v curves must intersect each other within the tolerance.
@@ -49,67 +49,6 @@ GEOML_API_EXPORT Handle(Geom_BSplineSurface)
 interpolate_curves(const std::vector<Handle(Geom_Curve)>& curves,
                    unsigned int max_degree=3,
                    bool join_continously=false);
-
-/**
- * @brief Create a revolving surface
- *
- * //@param profile_curve Profile curve (contained in a plane)
- * //@param rotation_axis The rotation axis
- * //@param angle The rotation angle (2*PI by default)
- */
-
-GEOML_API_EXPORT Handle(Geom_BSplineSurface)
-revolving_surface(const Handle(Geom_BSplineCurve)& profile_curve,
-                  const gp_Ax1& rotation_axis,
-                  const Standard_Real angle=2*M_PI); // check if it works to choose another
-                                                     // angle
-
-/**
- * @brief Create a B-spline surface
- *
- * //@param control_points A 2-dimensional control point net
- * //@param weights A 2-dimensional weights net 
- * //@param U_knots The knot vector in U-direction
- * //@param V_knots The knot vector in V-direction
- * //@param U_mults The multiplicities of the U-direction knots
- * //@param V_mults The multiplicities of the V-direction knots
- * //@param U_degree The degree in U-direction
- * //@param V_degree The degree in V-direction
- * //@param U_periodic Boolean flag for periodicity in U-direction
- * //@param V_periodic Boolean flag for periodicity in V-direction
- */
-GEOML_API_EXPORT Handle(Geom_BSplineSurface)
-b_spline_surface(const TColgp_Array2OfPnt &control_points, 
-                 const TColStd_Array2OfReal &weights, 
-                 const TColStd_Array1OfReal &U_knots, 
-                 const TColStd_Array1OfReal &V_knots, 
-                 const TColStd_Array1OfInteger &U_mults, 
-                 const TColStd_Array1OfInteger &V_mults, 
-                 const Standard_Integer U_degree, 
-                 const Standard_Integer V_degree, 
-                 const Standard_Boolean U_periodic=Standard_False, 
-                 const Standard_Boolean V_periodic=Standard_False);
-/*GEOML_API_EXPORT Handle(Geom_BSplineSurface)
-b_spline_surface(const std::vector< std::vector<gp_Pnt> > & control_points, 
-                 const std::vector< std::vector<double> >  & weights, 
-                 const std::vector<double> & U_knots, 
-                 const std::vector<double> & V_knots, 
-                 const std::vector<int> & U_mults, 
-                 const std::vector<int>  & V_mults, 
-                 const Standard_Integer U_degree, 
-                 const Standard_Integer V_degree, 
-                 const Standard_Boolean U_periodic=Standard_False, 
-                 const Standard_Boolean V_periodic=Standard_False);*/                
-
-
-/**
- * @brief Create a surface from 4 points
- *
- * //@param points The corner points of the surface
- */
-
-GEOML_API_EXPORT Handle(Geom_BSplineSurface)
-surface_from_4_points (const std::vector<gp_Pnt> & points);
  
 
 } // namespace geoml
