e15702a736c8bbce14e1aa9d74d863ef6e0aec99
Anton Reiswich <anton.reiswich@dlr.de>
2024-04-17 14:31:20 +0200

    rename filie with 2d collection and rename its class to List2d

---
+++ src/geometry/List2d.h
@@ -0,0 +1,62 @@
+/* 
+* Copyright (C) 2024 German Aerospace Center (DLR/SC)
+*
+* Created: 2024-04-10 Anton Reiswich <Anton.Reiswich@dlr.de>
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+/**
+* @file
+* @brief  Implementation of a 2 dimensional data structure.
+*/
+#pragma once
+
+#include "geoml_internal.h"
+
+#include <vector>
+
+namespace geoml 
+{
+
+template<typename T>
+class List2d
+{
+public:
+
+    List2d(int rows, int cols);
+
+    // 2d_list(std::vector<std::vector<T>> 2d_vec);
+
+    T& at(int row, int col);
+
+    const T& at(int row, int col); 
+
+    T& operator()(int row, int col);
+
+    const T& operator()(int row, int col) const;
+
+    void add(int row, int col, T input);
+
+    int numRows() const;
+
+    int numCols() const;
+
+private:
+
+    int m_rows;
+    int m_cols;
+    std::vector<T> flat_data;
+
+};
+
+} // end namespace geoml
+
