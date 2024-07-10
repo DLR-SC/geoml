f7f83e50e3beeea45bd096dbc6265aac160724a1
Anton Reiswich <anton.reiswich@dlr.de>
2024-07-10 21:47:51 +0200

    clean up api tests

--- tests/apitests/src/testCurveModeling.cpp
+++ tests/apitests/src/testCurveModeling.cpp
@@ -11,234 +11,195 @@
 #include <GeomAdaptor_Curve.hxx>
 #include <GCPnts_AbscissaPoint.hxx>
 
-/////////
-#include "STEPControl_Writer.hxx"
-#include <BRepBuilderAPI_MakeFace.hxx>
-
-void writeToStp(Handle_Geom_Curve cur, std::string const& name_file)
-{
-    BRepBuilderAPI_MakeEdge edgeMaker;
-    edgeMaker.Init(cur);
-    TopoDS_Shape edge = edgeMaker.Shape();
-
-    STEPControl_Writer aStepWriter;
-    aStepWriter.Transfer(edge,STEPControl_AsIs);
-    aStepWriter.Write(name_file.c_str());
-} 
-
-void writeGeomEntityToStepFile(Handle_Geom_Surface surface, std::string fileName)
-{
-	BRepBuilderAPI_MakeFace faceMaker;
-	faceMaker.Init(surface,true,1E-6);	
-	TopoDS_Shape face = faceMaker.Shape();
-
-	STEPControl_Writer writer;
-	writer.Transfer(face,STEPControl_AsIs);
-	writer.Write(fileName.c_str());
-	return;
-}
-
-void writeGeomEntityToStepFile(const TopoDS_Shape& my_shape, std::string fileName)
-{
-    STEPControl_Writer writer;
-    writer.Transfer(my_shape,STEPControl_AsIs);
-    writer.Write(fileName.c_str());
-}
-/////////
-
-
 TEST(Test_nurbs_curve, closed_unclamped_nurbs_curve)
 {    
+    // define a closed B-spline curve of 
+    // degree 2 by overlapping two control points (same number of overlapping control points as the degree) and 
+    // a uniform choice of knots      
 
-// define a closed B-spline curve of 
-// degree 2 by overlapping two control points (same number of overlapping control points as the degree) and 
-// a uniform choice of knots      
+    // define control points
+    gp_Pnt p_1(0.0, 0.0, 0.0);
+    gp_Pnt p_2(0.0, 1.0, 2.0);
+    gp_Pnt p_3(0.0, 2.0, -1.0);
+    gp_Pnt p_4(0.0, 0.0, 0.0);
+    gp_Pnt p_5(0.0, 1.0, 2.0);
 
-// define control points
-gp_Pnt p_1(0.0, 0.0, 0.0);
-gp_Pnt p_2(0.0, 1.0, 2.0);
-gp_Pnt p_3(0.0, 2.0, -1.0);
-gp_Pnt p_4(0.0, 0.0, 0.0);
-gp_Pnt p_5(0.0, 1.0, 2.0);
+    std::vector<gp_Pnt> control_points {p_1,p_2,p_3,p_4,p_5};
 
-std::vector<gp_Pnt> control_points {p_1,p_2,p_3,p_4,p_5};
+    // degree:
+    Standard_Integer degree = 2;
 
-// degree:
-Standard_Integer degree = 2;
+    // weights (non-rational):
+    std::vector<Standard_Real> weights (5,1.0);
 
-// weights (non-rational):
-std::vector<Standard_Real> weights (5,1.0);
+    // knots: note: as the number of knots (m+1), the number of control points (p+1) and the degree p
+    // are related by m = n + p +1, the number of knots has to be 8 in this case.
 
-// knots: note: as the number of knots (m+1), the number of control points (p+1) and the degree p
-// are related by m = n + p +1, the number of knots has to be 8 in this case.
+    std::vector<Standard_Real> knots {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
 
-std::vector<Standard_Real> knots {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+    // multiplicities: 
+    std::vector<int> mults(8,1);
 
-// multiplicities: 
-std::vector<int> mults(8,1);
+    Handle(Geom_BSplineCurve) curve =
+        geoml::nurbs_curve(
+            control_points, 
+            weights, 
+            knots, 
+            mults, 
+            degree);
 
-Handle(Geom_BSplineCurve) curve =
-    geoml::nurbs_curve(
-        control_points, 
-        weights, 
-        knots, 
-        mults, 
-        degree);
+    // expect the start and end point of the curve to coincide
+    EXPECT_NEAR(
+        curve->StartPoint().X(), 
+        curve->EndPoint().X(), 
+        1e-5);
 
-// expect the start and end point of the curve to coincide
-EXPECT_NEAR(
-    curve->StartPoint().X(), 
-    curve->EndPoint().X(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->StartPoint().Y(), 
+        curve->EndPoint().Y(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->StartPoint().Y(), 
-    curve->EndPoint().Y(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->StartPoint().Z(), 
+        curve->EndPoint().Z(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->StartPoint().Z(), 
-    curve->EndPoint().Z(), 
-    1e-5);
-
-// expect the curve to be non-rational, as all weights are set to 1
-EXPECT_EQ(curve->IsRational(), false); 
+    // expect the curve to be non-rational, as all weights are set to 1
+    EXPECT_EQ(curve->IsRational(), false); 
 
 }
 
 TEST(Test_nurbs_curve, open_clamped_nurbs_curve)
 {
+    // define an open and clamped NURBS curve of degree 2
 
-// define an open and clamped NURBS curve of degree 2
+    // control points
+    gp_Pnt p1(0.0, 0.0, 0.0);
+    gp_Pnt p2(0.0, 1.0, 2.0);
+    gp_Pnt p3(0.0, 2.0, -1.0);
+    gp_Pnt p4(1.0, 0.0, 3.0);
+    gp_Pnt p5(0.0, 4.0, 0.0);
 
-// control points
-gp_Pnt p1(0.0, 0.0, 0.0);
-gp_Pnt p2(0.0, 1.0, 2.0);
-gp_Pnt p3(0.0, 2.0, -1.0);
-gp_Pnt p4(1.0, 0.0, 3.0);
-gp_Pnt p5(0.0, 4.0, 0.0);
+    std::vector<gp_Pnt> control_points {p1, p2, p3, p4, p5};
 
-std::vector<gp_Pnt> control_points {p1, p2, p3, p4, p5};
+    // degree:
+    Standard_Integer degree = 2;
 
-// degree:
-Standard_Integer degree = 2;
+    // weights:
+    std::vector<Standard_Real> weights(5,1.0);
 
-// weights:
-std::vector<Standard_Real> weights(5,1.0);
+    // knots: note: as the number of knots (m+1), the number of control points (n+1) and the degree p
+    // are related by m = n + p +1, the number of knots has to be 8 in this case. To achive this, in the following the
+    // first and the last knot have to appear trice (have multiplicity of 3)
 
-// knots: note: as the number of knots (m+1), the number of control points (n+1) and the degree p
-// are related by m = n + p +1, the number of knots has to be 8 in this case. To achive this, in the following the
-// first and the last knot have to appear trice (have multiplicity of 3)
+    std::vector<Standard_Real> knots{0.0, 1.0, 2.0, 3.0};
 
-std::vector<Standard_Real> knots{0.0, 1.0, 2.0, 3.0};
+    // multiplicities: 
+    std::vector<int> mults {3, 1, 1, 3};
 
-// multiplicities: 
-std::vector<int> mults {3, 1, 1, 3};
+    Handle(Geom_BSplineCurve) curve =
+        geoml::nurbs_curve(
+            control_points, 
+            weights, 
+            knots, 
+            mults, 
+            degree);
 
-Handle(Geom_BSplineCurve) curve =
-    geoml::nurbs_curve(
-        control_points, 
-        weights, 
-        knots, 
-        mults, 
-        degree);
+    EXPECT_NEAR(
+        curve->StartPoint().X(), 
+        p1.X(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->StartPoint().X(), 
-    p1.X(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->StartPoint().Y(), 
+        p1.Y(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->StartPoint().Y(), 
-    p1.Y(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->StartPoint().Z(), 
+        p1.Z(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->StartPoint().Z(), 
-    p1.Z(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->EndPoint().X(), 
+        p5.X(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->EndPoint().X(), 
-    p5.X(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->EndPoint().Y(), 
+        p5.Y(), 
+        1e-5);
 
-EXPECT_NEAR(
-    curve->EndPoint().Y(), 
-    p5.Y(), 
-    1e-5);
-
-EXPECT_NEAR(
-    curve->EndPoint().Z(), 
-    p5.Z(), 
-    1e-5);
+    EXPECT_NEAR(
+        curve->EndPoint().Z(), 
+        p5.Z(), 
+        1e-5);
 
 }
 
 TEST(Test_nurbs_curve, circle_rational_bspline_three_arcs)
 {
+    // define a circle via a rational B-spline curve
+    // of degree 2:
 
-// define a circle via a rational B-spline curve
-// of degree 2:
+    // control points
+    gp_Pnt p_1(0.0, 0.0, 2.0);
+    gp_Pnt p_2(0.0, 3.464, 2.0);
+    gp_Pnt p_3(0.0, 1.732, -1.0);
+    gp_Pnt p_4(0.0, 0.0, -4.0);
+    gp_Pnt p_5(0.0, -1.732, -1.0);
+    gp_Pnt p_6(0.0, -3.464, 2.0);
+    gp_Pnt p_7(0.0, 0.0, 2.0);
 
-// control points
-gp_Pnt p_1(0.0, 0.0, 2.0);
-gp_Pnt p_2(0.0, 3.464, 2.0);
-gp_Pnt p_3(0.0, 1.732, -1.0);
-gp_Pnt p_4(0.0, 0.0, -4.0);
-gp_Pnt p_5(0.0, -1.732, -1.0);
-gp_Pnt p_6(0.0, -3.464, 2.0);
-gp_Pnt p_7(0.0, 0.0, 2.0);
+    std::vector<gp_Pnt> control_points {p_1, p_2, p_3, p_4, p_5, p_6, p_7};
+    
+    // degree:
+    Standard_Integer degree = 2;
 
-std::vector<gp_Pnt> control_points {p_1, p_2, p_3, p_4, p_5, p_6, p_7};
-  
-// degree:
-Standard_Integer degree = 2;
+    // weights:
+    std::vector<Standard_Real> weights {1.0, 0.5, 1.0, 0.5, 1.0, 0.5, 1.0};
 
-// weights:
-std::vector<Standard_Real> weights {1.0, 0.5, 1.0, 0.5, 1.0, 0.5, 1.0};
+    // knots: (we need 10 knots (including their multiplicities), which follows from "#knots = #control_points + degree + 1"
+    std::vector<Standard_Real> knots {0.0, 1.0, 2.0, 3.0};
 
-// knots: (we need 10 knots (including their multiplicities), which follows from "#knots = #control_points + degree + 1"
-std::vector<Standard_Real> knots {0.0, 1.0, 2.0, 3.0};
+    // multiplicities: 
+    std::vector<int> mults {3, 2, 2, 3};
 
-// multiplicities: 
-std::vector<int> mults {3, 2, 2, 3};
+    Handle(Geom_BSplineCurve) curve =
+        geoml::nurbs_curve(
+            control_points, 
+            weights, 
+            knots, 
+            mults, 
+            degree);
 
-Handle(Geom_BSplineCurve) curve =
-    geoml::nurbs_curve(
-        control_points, 
-        weights, 
-        knots, 
-        mults, 
-        degree);
+    // get the length of the curve
+    Standard_Real umin = curve->FirstParameter();
+    Standard_Real umax = curve->LastParameter();
+    GeomAdaptor_Curve adaptorCurve(curve, umin, umax);
+    Standard_Real length = GCPnts_AbscissaPoint::Length(adaptorCurve, umin, umax);
 
-// get the length of the curve
-Standard_Real umin = curve->FirstParameter();
-Standard_Real umax = curve->LastParameter();
-GeomAdaptor_Curve adaptorCurve(curve, umin, umax);
-Standard_Real length = GCPnts_AbscissaPoint::Length(adaptorCurve, umin, umax);
+    // Expect the circumference of the circle.
+    EXPECT_NEAR(length, 12.566, 1e-3);
+    EXPECT_EQ(curve->NbPoles(), 7);
+    EXPECT_EQ(curve->NbKnots(), 4); // note, the number of knots OCC gives is not counting the multiplicities of the knots!
+    EXPECT_FALSE(curve->IsPeriodic());
 
-// Expect the circumference of the circle.
-EXPECT_NEAR(length, 12.566, 1e-3);
-EXPECT_EQ(curve->NbPoles(), 7);
-EXPECT_EQ(curve->NbKnots(), 4); // note, the number of knots OCC gives is not counting the multiplicities of the knots!
-EXPECT_FALSE(curve->IsPeriodic());
+    EXPECT_EQ(curve->Multiplicity(1), 3);
+    EXPECT_EQ(curve->Multiplicity(2), 2);
+    EXPECT_EQ(curve->Multiplicity(3), 2);
+    EXPECT_EQ(curve->Multiplicity(4), 3);
 
-EXPECT_EQ(curve->Multiplicity(1), 3);
-EXPECT_EQ(curve->Multiplicity(2), 2);
-EXPECT_EQ(curve->Multiplicity(3), 2);
-EXPECT_EQ(curve->Multiplicity(4), 3);
+    curve->SetPeriodic();
 
-curve->SetPeriodic();
+    EXPECT_TRUE(curve->IsPeriodic());
+    EXPECT_EQ(curve->NbPoles(), 6); // the number of control point differs from the curve before envoking SetPeriodic() 
+    EXPECT_EQ(curve->NbKnots(), 4);
 
-EXPECT_TRUE(curve->IsPeriodic());
-EXPECT_EQ(curve->NbPoles(), 6); // the number of control point differs from the curve before envoking SetPeriodic() 
-EXPECT_EQ(curve->NbKnots(), 4);
-
-EXPECT_EQ(curve->Multiplicity(1), 2); // the multiplicities differ, too. 
-EXPECT_EQ(curve->Multiplicity(2), 2);
-EXPECT_EQ(curve->Multiplicity(3), 2);
-EXPECT_EQ(curve->Multiplicity(4), 2);
+    EXPECT_EQ(curve->Multiplicity(1), 2); // the multiplicities differ, too. 
+    EXPECT_EQ(curve->Multiplicity(2), 2);
+    EXPECT_EQ(curve->Multiplicity(3), 2);
+    EXPECT_EQ(curve->Multiplicity(4), 2);
 
 }
 
