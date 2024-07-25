b3c3a30f82aa42ba2219af3a13a7405082a3fcf5
Anton Reiswich <anton.reiswich@dlr.de>
2024-07-25 13:01:54 +0200

    add implementation for tranlate function for std::vector of gp_Pnts

--- src/geoml/transformations/transformations.cpp
+++ src/geoml/transformations/transformations.cpp
@@ -8,13 +8,28 @@ namespace geoml{
 TopoDS_Shape translate(TopoDS_Shape const& origin, gp_Vec const& direction, double factor)
 {
     auto t = Transformation(direction*factor);
+    
     return t.Transform(origin);
 }
 
 gp_Pnt translate(gp_Pnt const& origin, gp_Vec const& direction, double factor)
 {
     auto t = Transformation(direction*factor);
+
     return t.Transform(origin);
+}
+
+std::vector<gp_Pnt> translate(std::vector<gp_Pnt> const& origin, gp_Vec const& direction, double factor)
+{
+    auto t = Transformation(direction*factor);
+
+    std::vector<gp_Pnt> transformed_points;
+
+    for(gp_Pnt p: origin){
+        transformed_points.push_back(t.Transform(p));
+    }
+
+    return transformed_points;
 }
 
 } // namespace geoml
