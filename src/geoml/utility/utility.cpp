9ec782009bae76239348b250e752900ad66d86a6
Anton Reiswich <anton.reiswich@dlr.de>
2024-09-16 15:31:45 +0200

    add check_if_two_points_equal and flip_curve functions

--- src/geoml/utility/utility.cpp
+++ src/geoml/utility/utility.cpp
@@ -9,4 +9,14 @@ Array2d<gp_Pnt> extract_control_points_surface(const Handle(Geom_BSplineSurface)
     return TCol_to_Array2d(b_spline_surface->Poles());
 }
 
+bool check_if_two_points_equal(gp_Pnt const& point_1, gp_Pnt const& point_2)
+{
+   return point_1.IsEqual(point_2, 1e-5);
+}
+
+Handle(Geom_Curve) flip_curve(Handle(Geom_Curve) const& curve, bool flip_me)
+{
+    return curve->Reversed();
+}
+
 } // namespace geoml
--- src/geoml/utility/utility.h
+++ src/geoml/utility/utility.h
@@ -23,5 +23,22 @@ namespace geoml
 GEOML_API_EXPORT Array2d<gp_Pnt> 
 extract_control_points_surface(const Handle(Geom_BSplineSurface) &b_spline_surface);
 
+/**
+ * @brief Check it two points are equal
+ * 
+ * @param point_1 First point
+ * @param point_2 Second point
+ */
+GEOML_API_EXPORT bool check_if_two_points_equal(gp_Pnt const& point_1, gp_Pnt const& point_2);
+
+/**
+ * @brief Flips the parameter direction of a curve if argument flip_me equals true
+ * 
+ * @param curve A handle on a Geom_Curve
+ * @param flip_me Boolean value determining if the curve is to flip
+ */
+GEOML_API_EXPORT Handle(Geom_Curve) flip_curve(Handle(Geom_Curve) const& curve, bool flip_me);
+
+
 
 } // namespace geoml
