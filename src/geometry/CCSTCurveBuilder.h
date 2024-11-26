de929c4fe1593dd8e81ce17d9c9a3ec67f0bd396
Anton Reiswich <anton.reiswich@dlr.de>
2024-11-26 15:31:16 +0100

    add CCSTCurveBuilder class

---
+++ src/geometry/CCSTCurveBuilder.h
@@ -0,0 +1,55 @@
+/* 
+* Copyright (C) 2007-2014 German Aerospace Center (DLR/SC)
+*
+* Created: 2014-11-17 Martin Siggel <Martin.Siggel@dlr.de>
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef CCSTCURVEBUILDER_H
+#define CCSTCURVEBUILDER_H
+
+#include "geoml_internal.h"
+
+
+#include <vector>
+
+#include <Geom_BSplineCurve.hxx>
+
+namespace geoml
+{
+
+/**
+ * @brief The CCSTCurveBuilder class generates a B-Spline Curve from a CST parameterization
+ */
+class CCSTCurveBuilder
+{
+public:
+    GEOML_EXPORT CCSTCurveBuilder(double N1, double N2, const std::vector<double>& B, double T);
+
+    // returns parameters of cst curve
+    GEOML_EXPORT double N1() const;
+    GEOML_EXPORT double N2() const;
+    GEOML_EXPORT std::vector<double> B() const;
+    GEOML_EXPORT double T() const;
+
+    GEOML_EXPORT Handle(Geom_BSplineCurve) Curve();
+
+private:
+    double _n1, _n2, _t;
+    std::vector<double> _b;
+};
+
+} // namespace geoml
+
+#endif // CCSTCURVEBUILDER_H
